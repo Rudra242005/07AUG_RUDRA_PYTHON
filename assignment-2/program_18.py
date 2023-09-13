89864c7c1b0fad0fca46ec1199a5f292d490d464
rudra242005 <pandyarudra902@gmail.com>
2023-09-13 23:31:55 +0530

    update

--- assignment-2/program_18.py
+++ assignment-2/program_18.py
@@ -1,12 +1,14 @@
 #Write a Python function to reverses a string if its length is a multiple of 4.
 
+str = input("enter a string : ")
 
-def mystr():
-    str = input("enter a string : ")
-    i = len(str)
-    if (i % 4) == 0 :
-        print(str[-1:0])
-    else:
-        print(str)
+def mystr(getdata):
     
-mystr()
+   if len(getdata) % 4 ==0:
+      return getdata[::-1]
+   else:
+      return getdata
+   
+x = mystr(getdata = str)
+print(x)    
+
