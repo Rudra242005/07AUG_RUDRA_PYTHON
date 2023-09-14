80c21603e09bf28e1ae8d76ae9a2148d83086e1e
rudra242005 <pandyarudra902@gmail.com>
2023-09-14 11:10:00 +0530

    update

--- assignment-2/program_20.py
+++ assignment-2/program_20.py
@@ -1,3 +1,6 @@
 #Write a Python function to insert a string in the middle of a string.
 
-
+str = ("i am  student")
+sub = input("enter second string to add it in middele : ")
+a = str[0:5] + sub + str[5:]
+print(a)
