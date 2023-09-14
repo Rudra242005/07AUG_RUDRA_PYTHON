d3d97811930fbcd7b977f3f24d51a24b32a66097
rudra242005 <pandyarudra902@gmail.com>
2023-09-14 14:13:13 +0530

    update

--- assignment-2/program_11.py
+++ assignment-2/program_11.py
@@ -1,6 +1,16 @@
 #Write a Python program to count the number of characters (character frequency) in a string
 
+str = input("Enter a string: ")
 
+count = {}
 
+for char in str:
 
+    if char in count:
+        count[char] += 1
+    else:
+        count[char] = 1
+
+for char, count in count.items():
+    print(f"'{char}': {count}")
 
--- assignment-2/program_16.py
+++ assignment-2/program_16.py
@@ -2,3 +2,19 @@
 'not' and 'poor' from a given string, if 'not' follows the 'poor', replace the
 whole 'not'...'poor' substring with 'good'. Return the resulting string."""
 
+a = ''' the state for any person of being extremely poor. It is the not extreme situation when
+     a person feels lack of essential items required to continue the life such as shelter, 
+     adequate food, clothing, medicines, etc.'''
+
+
+# sub_not = "not"
+# sub_poor = "poor"
+
+not_occ = a.find('not')
+poor_occ = a.find('poor')
+
+print(not_occ)
+print(poor_occ)
+
+if not_occ > poor_occ :
+    
